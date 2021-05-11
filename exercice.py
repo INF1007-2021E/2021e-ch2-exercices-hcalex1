48b562ed8c4f61e068c44cb43782fca6286ee45f
Alex HC <hcalex1.accounts@hotmail.com>
2021-05-11 15:27:05 -0400

    solution

--- exercice.py
+++ exercice.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     # TODO completer la fonction ici
+    MOT = ''
+    for lettre in mot:
+        MOT += chr(ord(lettre)-32)
+    mot = MOT
     return mot
-
-test = "test"
 
 if __name__ == '__main__':
     mots = [
